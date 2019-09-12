87eaea9a854567be3a1dd7b60c10a6023a5bc28b
Daw-Ran Liou <dawran6@gmail.com>
2019-09-12 12:23:00 -0700

    Trim project - remove nrepl alias

--- bin/repl.clj
+++
@@ -1,8 +0,0 @@
-(ns repl
-  (:require [nrepl.server]))
-
-(let [port (or (some-> (first *command-line-args*)
-                       (java.lang.Long/parseLong))
-               7888)]
-  (nrepl.server/start-server :port port)
-  (println "Started nREPL on port" port))
