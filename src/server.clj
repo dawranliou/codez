3163a53533edf3f5523b7746d128ad994f6d483b
Daw-Ran Liou <dawran6@gmail.com>
2019-09-13 08:36:15 -0700

    Don't run another nrepl in dev mode

--- src/server.clj
+++ src/server.clj
@@ -1,18 +1,20 @@
 (ns server
   (:require [coast]
+            [coast.env :as env]
             [routes]
             [nrepl.server :refer [start-server stop-server]])
   (:gen-class))
-
-(defonce nrepl-server
-  (do
-    (println "nrepl starting at port 7889")
-    (start-server :port 7889)))
 
 (def app (coast/app {:routes routes/routes}))
 
 (defn -main [& [port]]
   (coast/server app {:port port}))
+
+(defonce nrepl-server (if (= "dev" (env/env :coast-env))
+                        nil
+                        (do
+                          (println "Starting nrepl server at port 7889")
+                          (start-server :port 7889))))
 
 (comment
   (-main))
